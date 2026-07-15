6d9e4900874901d41c31d4b127740a28f1342b1f
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-15 06:44:31 +0000

    Add city selection and user data display to NgForDirective component

--- 14_07_2026/angular-13/src/app/components/Directives/StructuralDirectives/ng-for-directive/ng-for-directive.component.ts
+++ 14_07_2026/angular-13/src/app/components/Directives/StructuralDirectives/ng-for-directive/ng-for-directive.component.ts
@@ -8,6 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class NgForDirectiveComponent implements OnInit {
 
   courses:string[]=["C","Java","HTML","CSS"]
+  citys: string[]=['Kolkata', 'Pune',"Bengalore",'Mumbai'];
+
+  users:any[] = [
+  {
+    id: 101,
+    name: 'Ram',
+    address: 'Bangalore',
+    isActive: true
+  },
+  {
+    id: 102,
+    name: 'Shyam',
+    address: 'Mumbai',
+    isActive: false
+  },
+  {
+    id: 103,
+    name: 'Amit',
+    address: 'Delhi',
+    isActive: true
+  },
+  {
+    id: 104,
+    name: 'Priya',
+    address: 'Kolkata',
+    isActive: true
+  },
+  {
+    id: 105,
+    name: 'Neha',
+    address: 'Chennai',
+    isActive: false
+  }
+];
 
   constructor() { }
 
