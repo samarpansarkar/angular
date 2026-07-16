8d980ef6f38a4d0d6cda783152b938defe9f2218
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-16 04:47:55 +0000

    feat: add routing and components for admin and app; update app component with navigation links
    - Implemented routing in app.routes.ts for the main app and admin components.
    - Updated app.ts to include CommonModule and added navigation links.
    - Created new README.md files for various sections in the Angular complete guide.

--- 15_07_2026/angular_routing/src/app/app.routes.ts
+++ 15_07_2026/angular_routing/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Admin } from './admin/admin';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:App
+    },
+    {
+        path:"admin",
+        component:Admin
+    }
+];
--- 15_07_2026/angular_routing/src/app/app.ts
+++ 15_07_2026/angular_routing/src/app/app.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular_routing');
+
+  navLinks:string[]=["introduction","Installation", "Data Binding", "Admin"]
 }
