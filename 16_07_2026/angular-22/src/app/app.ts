6bc6b70ac2eb5fcdfedeeee4e38185272d420797
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-16 10:52:01 +0000

    feat: add footer component and integrate it into the app layout

--- 16_07_2026/angular-22/src/app/app.ts
+++ 16_07_2026/angular-22/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar,RouterOutlet],
+  imports: [Navbar, Footer ,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
