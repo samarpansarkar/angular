fde90ed5b9c7857db7e18e2ab2850646babc10ee
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-17 09:19:44 +0000

    feat: enhance template forms with validation messages and reset functionality

--- AngularTopicsPractical/src/app/pages/forms/template-forms/template-forms.ts
+++ AngularTopicsPractical/src/app/pages/forms/template-forms/template-forms.ts
@@ -1,30 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-forms',
   standalone:true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './template-forms.html',
   styleUrl: './template-forms.css',
 })
 export class TemplateForms {
   studentObj = {
-  name: '',
-  email: '',
-  password: '',
-  phoneNumber: '',
-  age: null,
-  DOB: '',
-  gender: '',
-};
-
- alertObject:any 
-
+    name: '',
+    email: '',
+    password: '',
+    phoneNumber: '',
+    age: null,
+    DOB: '',
+    gender: '',
+  };
+  alertObject:any 
   onSubmit(form: any) {
-  console.log(form.value);
-  console.log(this.studentObj);
+    console.log(form.value);
+    console.log(this.studentObj);
 
-  alert(JSON.stringify(this.studentObj, null, 2));
-}
+    alert(JSON.stringify(this.studentObj, null, 2));
+  }
+
+  resetForm(){
+    this.studentObj={
+      name: '',
+      email: '',
+      password: '',
+      phoneNumber: '',
+      age: null,
+      DOB: '',
+      gender: '',
+    }
+  };
 }
