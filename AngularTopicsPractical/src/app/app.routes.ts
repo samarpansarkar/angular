30f2d22f2b55527ca84ec004b338e0a1d6cb10a8
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-17 05:47:42 +0000

    feat: add routing for reactive forms component

--- AngularTopicsPractical/src/app/app.routes.ts
+++ AngularTopicsPractical/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { TemplateForms } from './pages/forms/template-forms/template-forms';
+import { ReactiveForms } from './pages/forms/reactive-forms/reactive-forms';
 
 export const routes: Routes = [
     {
         path:'template-forms',
         component:TemplateForms,
         title:"Template Forms"
+    },
+    {
+        path:'reactive-forms',
+        component:ReactiveForms,
+        title:"Reactive Forms"
     }
 ];
