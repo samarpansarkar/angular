8d9acd174f903bc4a0c7db9f05a11dba1c794876
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-17 05:46:35 +0000

    feat: add reactive forms component with template and styles

---
+++ AngularTopicsPractical/src/app/pages/forms/reactive-forms/reactive-forms.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-forms',
+  imports: [],
+  templateUrl: './reactive-forms.html',
+  styleUrl: './reactive-forms.css',
+})
+export class ReactiveForms {}
