e517fd80b748e6c7ee36df7e52d23d3b4272613b
samarpansarkar <samarpansarkar209@gmail.com>
2026-07-19 07:13:14 +0000

    feat: add Control Flows component with routing and basic structure

--- AngularTopicsPractical/src/app/app.routes.ts
+++ AngularTopicsPractical/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { TemplateForms } from './pages/forms/template-forms/template-forms';
 import { ReactiveForms } from './pages/forms/reactive-forms/reactive-forms';
+import { ControlFlows } from './pages/control-flows/control-flows';
 
 export const routes: Routes = [
+    {
+        path:'control-flows',
+        component:ControlFlows,
+        title: "Control Flows"
+    },
     {
         path:'template-forms',
         component:TemplateForms,
---
+++ AngularTopicsPractical/src/app/pages/control-flows/control-flows.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flows',
+  imports: [],
+  templateUrl: './control-flows.html',
+  styleUrl: './control-flows.css',
+})
+export class ControlFlows {}
